8e13e3dac4de256ffacad6e24da83f4da1119283
Glenn Twiggs <info@glenntwiggs.com>
2020-05-22 15:06:24 -0500

    Initial checkin of Spring Starter Application

---
+++ src/main/java/com/glenntwiggs/slack/appsurface/SlackAppSurfaceApplication.java
@@ -0,0 +1,13 @@
+package com.glenntwiggs.slack.appsurface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackAppSurfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackAppSurfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glenntwiggs/slack/appsurface/SlackAppSurfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glenntwiggs.slack.appsurface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlackAppSurfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
